6f318341fef8b682550784f069bd04d0c1adbe84
ByeongSu Go <107296751+eclipse1228@users.noreply.github.com>
2023-05-21 02:31:36 +0900

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,29 +1,32 @@
+
+# 국민연금 2020, 2021 종목 자료 읽기
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
 df_2021 = pd.read_csv('2021.csv')
 df_2020 = pd.read_csv('2020.csv')
+#
 
-df_national_pension = list(sorted(set(df_2021['종목명']).intersection(set(df_2020['종목명']))))
-print(df_national_pension)
+# 
+df_krx = pd.read_csv('code.csv')                      # code.csv 파일은 한국 전체 주식종목 정보
+df_krx = df_krx[['한글 종목약명', '단축코드']]          
+df_krx.rename(columns={'한글 종목약명':'name','단축코드':'code'},inplace=True) # rename 하기 
 
-df_krx = pd.read_csv('code.csv')
-df_krx = df_krx[['한글 종목약명', '단축코드']]
-df_krx.rename(columns={'한글 종목약명':'name','단축코드':'code'},inplace=True)
+df_krx['code'] = df_krx['code'].astype(str).str.zfill(6) # zfill은 6자리로 구간을 넓히는것 빈자리는 0으로 채움 # , # astype(str) string 형식으로 변경 #
+df_krx = pd.DataFrame(df_krx)                            #  DataFrame으로~       
+df_code = df_krx.query(f"name in {df_national_pension}") # df_code에 df_krx에 df_national_pension의 name을 query함수 사용해서, 교집합을 찾아서 df_code 만든다.
+df_code = pd.DataFrame(df_code)                          # DataFrame으로 만들기
+#
 
-df_krx['code'] = df_krx['code'].astype(str).str.zfill(6)
-
-df_krx = pd.DataFrame(df_krx)
-df_code = df_krx.query(f"name in {df_national_pension}")
-df_code = pd.DataFrame(df_code)
-
+# 다운로드
 pip install lxml
 pip install html5lib
 pip install requests
+#
 
+# 
 import requests
-
+#
 def get_url(item_name, df_code):
     code = df_code.query("name=='{}'".format(item_name))['code'].to_string(index=False)
     url = 'http://finance.naver.com/item/sise_day.nhn?code={code}'.format(code=code)
@@ -54,6 +57,7 @@ for name in df_national_pension:
     data = df_price[df_price['날짜'] == '2021.01.04']
     data.insert(0,'name',name)
     df_2021_price = pd.concat([df_2021_price,data[['name','종가']]],ignore_index=True)
+    print(data)
     data = df_price[df_price['날짜'] == '2020.01.02']
     data.insert(0,'name',name)
     df_2020_price = pd.concat([df_2020_price,data[['name','종가']]],ignore_index=True)
@@ -63,20 +67,3 @@ pd.set_option('display.max_rows',100)
 df_result = pd.DataFrame()
 # df_result['result'] = df_2021_price['종가'] > df_2020_price['종가']
 
-df_2021_price = df_2021_price.drop(df_2021_price.index[df_2021_price['name']=='SK바이오팜'],axis=0)
-df_2021_price = df_2021_price.reset_index(drop=True)
-
-
-
-df_result = pd.DataFrame()
-df_result['result'] = (df_2021_price['종가'] > df_2020_price['종가']).astype(int)
-
-result_counts = df_result['result'].value_counts()
-
-# Plotting the bar graph
-plt.bar(result_counts.index, result_counts.values,color=['red', 'blue'])
-plt.xlabel('Result')
-plt.ylabel('Count')
-plt.xticks([0, 1], ['Down', 'Up'])
-plt.title('Comparison Result')
-plt.show()
